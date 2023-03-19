f104389bffe8ad6717a51844cb668c4c6d446a21
Goda Mačiuolytė <maciuolyte@gmail.com>
2023-03-19 13:52:54 +0200

    populated main page with games

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,19 +6,19 @@
     @include('layouts/navbar')
     <div class="container">
         <div class="row row-cols-1 row-cols-md-2 g-4">
+          @foreach($games as $game)
             <div class="col">
-                @foreach ($games as $game)
-                        <div class="col">
-                            <div class="card">
-                                <img src="..." class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{$game->game_name}}</h5>
-                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                </div>
-                                </div>
-                        </div>
-                    @endforeach
+              <div class="col">
+                  <div class="card">
+                      <img src="..." class="card-img-top" alt="...">
+                      <div class="card-body">
+                          <h5 class="card-title">{{$game->game_name}}</h5>
+                          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      </div>
+                  </div>
+              </div>
             </div>
+            @endforeach
           </div>
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GameController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('/', [GameController::class, 'index']);
