68c630ed6b5e13cb6223c136849f54555ff3c9b1
Goda Mačiuolytė <maciuolyte@gmail.com>
2023-03-19 20:04:15 +0200

    custom 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>404</title>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+            <h1 class="error">404</h2>
+            <h2 class="error">Oops! Something is wrong.</h1>
+            <h2 class="error">Go back</h1>
+    </div>
+</body>
+</html>
