b0e20624f0cd0c1a3efc436675466321a4552385
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2023-03-21 14:58:24 +0200

    persimetu darba i kita pc

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique();
+            $table->string('password');
             $table->float('balance')->unsigned()->default(0.0);
             $table->rememberToken();
             $table->timestamps();
