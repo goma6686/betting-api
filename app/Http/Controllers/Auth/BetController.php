578384a1022488422df80be4af572d46f58466f6
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2023-04-08 20:16:35 +0300

    test

--- app/Http/Controllers/Auth/BetController.php
+++ app/Http/Controllers/Auth/BetController.php
@@ -22,4 +22,36 @@ class BetController extends Controller
     public function issuetoken (User $user){
         return $user->createToken('token')->plainTextToken;
     }
+
+    public function ping(Request $request){
+        $xmlData = $request->getContent();
+        $xml = simplexml_load_string($xmlData);
+
+        $method = (string) $xml->method;
+        $token = (string) $xml->token;
+        $requestId = (string) $xml->request_id;
+        $time = (string) $xml->time;
+        $signature = (string) $xml->signature;
+        $params = (array) $xml->params;
+
+        $sig = hash_hmac('sha256', $requestId, 'CCHWS-ZIFJV-HEAOB-DV336');
+
+        $xmlResponse = new \SimpleXMLElement('<root/>');
+        $xmlResponse->addChild('response', $sig);
+        return response($xmlResponse->asXML())->header('Content-Type', 'application/xml');
+        /*if () {
+            $success = '1';
+            $error_code = '0';
+            $error_text = '';
+        } else {
+            $response = '<error>unknown method</error>';
+        }
+
+        $sig = hash_hmac('sha256', $requestId, 'CCHWS-ZIFJV-HEAOB-DV336');
+
+        $xmlResponse = new \SimpleXMLElement('<root/>');
+        $xmlResponse->addChild('response', $response);
+        
+        return response($xmlResponse->asXML())->header('Content-Type', 'application/xml');*/
+    }
 }
