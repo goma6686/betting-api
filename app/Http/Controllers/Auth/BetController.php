5e8ee6142114c82da9c8f60d6b6291bf43dccc56
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2023-04-09 12:48:54 +0300

    fixed ping method

--- app/Http/Controllers/Auth/BetController.php
+++ app/Http/Controllers/Auth/BetController.php
@@ -23,49 +23,4 @@ class BetController extends Controller
     public function issuetoken (User $user){
         return $user->createToken('token')->plainTextToken;
     }
-
-    public function test(){
-        $response['status'] = 'ok';
-        return response()->json($response);
-    }
-
-    public function ping(Request $request){
-        $xmlData = $request->getContent();
-        $xml = simplexml_load_string($xmlData);
-
-        $method = (string) $xml->method;
-        $token = (string) $xml->token;
-        $time = (string) $xml->time;
-        $params = (array) $xml->params;
-        $signature = (string) $xml->signature;
-
-        $secret = "CCHWS-ZIFJV-HEAOB-DV336";
-        $calc_string = "method" . $method . "token" . $token . "time" . $time . $secret;
-        if (md5($calc_string) === $signature){
-            $success = '1';
-            $error_code = '0';
-            $error_text = '';
-            $time = time();
-            //$time = '1423124663';
-            $signature = md5("method" . $method . "token" . $token . "success" . $success . "error_code" . $error_code . "error_texttime" . $time . $secret);
-        } else {
-            $success = '0';
-            $error_code = '1';
-            $error_text = 'wrong_signature';
-            $time = time();
-            //$time = '1423124663';
-            $signature = md5("method" . $method . "token" . $token . "success" . $success . "error_code" . $error_code . "error_texttime" . $time . $secret);
-        }
-
-        $xmlResponse = new \SimpleXMLElement('<root/>');
-        $xmlResponse->addChild('method', $method);
-        $xmlResponse->addChild('token', $token);
-        $xmlResponse->addChild('success', $success);
-        $xmlResponse->addChild('error_code', $error_code);
-        $xmlResponse->addChild('error_text', $error_text);
-        $xmlResponse->addChild('time', $time);
-        $xmlResponse->addChild('signature', $signature);
-
-        return response($xmlResponse->asXML())->header('Content-Type', 'application/xml');
-    }
 }
