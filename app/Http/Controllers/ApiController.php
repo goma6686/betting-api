414049502c1c9012cfbd0ab254da6345c4d6b19c
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2023-04-09 14:39:48 +0300

    little fix for ping

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -17,6 +17,7 @@ class ApiController extends Controller
         $requestId = (string) $xml->request_id;
         $time = (string) $xml->time;
         $signature = (string) $xml->signature;
+        $params = (string) $xml->params;
 
         $secret = "CCHWS-ZIFJV-HEAOB-DV336";
 
@@ -37,7 +38,9 @@ class ApiController extends Controller
         $xmlResponse->addChild('success', $success);
         $xmlResponse->addChild('error_code', $error_code);
         $xmlResponse->addChild('error_text', $error_text);
-        $xmlResponse->addChild('time', $time);
+        $xmlResponse->addChild('params', $params);
+        $xmlResponse->addChild('response_id', $requestId);
+        $xmlResponse->addChild('time', time());
         $xmlResponse->addChild('signature', $signature);
 
         return response($xmlResponse->asXML())->header('Content-Type', 'application/xml');
@@ -46,14 +49,14 @@ class ApiController extends Controller
     public function ping($method, $secret, $requestId, $signature, $success, $error_code, $error_text, $calc_string){
 
         if (hash_hmac('sha256', $requestId, $secret) === $signature){
-            $responseId = hash_hmac('md5', $calc_string , $secret);
-            $signature = hash_hmac('sha256', $responseId, $secret);
+            $requestId = hash_hmac('md5', $calc_string , $secret);
+            $signature = hash_hmac('sha256', $requestId, $secret);
         } else {
             $success = '0';
             $error_code = '1';
             $error_text = 'wrong_signature';
-            $responseId = hash_hmac('md5', $calc_string , $secret);
-            $signature = hash_hmac('sha256', $responseId, $secret);
+            $requestId = hash_hmac('md5', $calc_string , $secret);
+            $signature = hash_hmac('sha256', $requestId, $secret);
         }
         return array($method, $secret, $requestId, $signature, $success, $error_code, $error_text);
     }
